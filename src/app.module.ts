e6a2cb6af01b394a1c99170f6650ed6ef116c6c2
zhoudenghui <15690329227@z63.com>
2023-12-13 10:52:19 +0800

    1

---
+++ src/app.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import * as dotenv from 'dotenv';
+import * as Joi from 'joi';
+
+const envFilePath = `.env.${process.env.NODE_ENV || `development`}`;
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath,
+      load: [() => dotenv.config({ path: '.env' })],
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'production')
+          .default('development'),
+        DB_PORT: Joi.number().default(3306),
+        DB_URL: Joi.string().domain(),
+        DB_HOST: Joi.string().ip(),
+      }),
+    }),
+    UserModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
